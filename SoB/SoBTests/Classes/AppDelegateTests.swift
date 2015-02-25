c1c2c79fd39de336bf87019ca20fb2d4bd76596f
Carlos Manzanas Navia <carlos.manzanas@intelygenz.com>
2015-02-25 21:40:22 +0100

    Add AppDelegateTests.

---
+++ SoB/SoBTests/Classes/AppDelegateTests.swift
@@ -0,0 +1,25 @@
+//
+//  AppDelegateTests.swift
+//  SoB
+//
+//  Created by Carlos Manzanas Navia on 25/2/15.
+//  Copyright (c) 2015 Carlos Manzanas Navia. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class AppDelegateTests: XCTestCase {
+
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+
+    
+}
